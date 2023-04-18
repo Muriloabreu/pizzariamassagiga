73896ac76f4e08bbf809e5c155cdbcff7857c9a4
muriloabreu <murilo.lilo@hotmail.com>
2023-04-18 20:22:23 -0300

    Inicialização Spring boot

---
+++ src/main/java/com/api/pizzariamassagiga/PizzariaMassaGigaApplication.java
@@ -0,0 +1,13 @@
+package com.api.pizzariamassagiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaMassaGigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaMassaGigaApplication.class, args);
+	}
+
+}
