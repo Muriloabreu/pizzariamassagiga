f7c889eaf4b4b037ec5895f99255be231f243388
muriloabreu <murilo.lilo@hotmail.com>
2023-04-19 22:12:51 -0300

    Implementação e modificação na Package Dtos

---
+++ src/main/java/com/api/pizzariamassagiga/dtos/BebidasDtos.java
@@ -0,0 +1,81 @@
+package com.api.pizzariamassagiga.dtos;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class BebidasDtos {
+	
+	@NotBlank
+	private String nome;
+	@NotNull
+	private int quantidade;
+	@NotNull
+	private Double valorUnitario;
+	@NotNull
+	private Double valorTotal;
+	
+	
+	/*Construtor*/
+	
+	public BebidasDtos() {
+		super();
+	}
+
+
+	public BebidasDtos(@NotBlank String nome, @NotNull int quantidade, @NotNull Double valorUnitario, @NotNull Double valorTotal) {
+		super();
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.valorUnitario = valorUnitario;
+		this.valorTotal = valorTotal;
+	}
+	
+	
+	/*Métodos Acessores*/
+
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+	public Double getValorUnitario() {
+		return valorUnitario;
+	}
+
+
+	public void setValorUnitario(Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	
+	
+	
+	
+	
+
+}
