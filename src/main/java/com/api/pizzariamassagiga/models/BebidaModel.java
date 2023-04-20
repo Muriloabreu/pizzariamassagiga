4b9772a90823afbe51f9f2d2dcd2295df790ef5a
muriloabreu <murilo.lilo@hotmail.com>
2023-04-19 22:03:05 -0300

    Modificação na Classe Model BebidaModel, atributos e metodos

--- src/main/java/com/api/pizzariamassagiga/models/BebidaModel.java
+++ src/main/java/com/api/pizzariamassagiga/models/BebidaModel.java
@@ -1,5 +1,7 @@
 package com.api.pizzariamassagiga.models;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,11 +17,112 @@ public class BebidaModel {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
 	private int quantidade;
 	@Column(nullable = false)
 	private Double valorUnitario;
 	@Column(nullable = false)
 	private Double valorTotal;
+	
+	/*Construtor*/
+	
+	public BebidaModel() {
+		super();
+	}
+
+	public BebidaModel(Long id, String nome ,int quantidade, Double valorUnitario, Double valorTotal) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.valorUnitario = valorUnitario;
+		this.valorTotal = valorTotal;
+	}
+
+	
+	/*Métodos Acessores*/
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public Double getValorTotal() {
+		
+		
+		Double soma;
+		
+		soma = getQuantidade() * getValorUnitario();
+		
+		
+		return soma;
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	@Override
+	public String toString() {
+		return "BebidaModel [id=" + id +"nome=" + nome + ", quantidade=" + quantidade + ", valorUnitario=" + valorUnitario
+				+ ", valorTotal=" + valorTotal + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome, quantidade, valorTotal, valorUnitario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BebidaModel other = (BebidaModel) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome) && quantidade == other.quantidade
+				&& Objects.equals(valorTotal, other.valorTotal) && Objects.equals(valorUnitario, other.valorUnitario);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
