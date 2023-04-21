b69d1aa39626598b3dfb3adb9e5df8013f27b179
muriloabreu <murilo.lilo@hotmail.com>
2023-04-20 21:35:04 -0300

    Implementação Interface RefeicaoRepository

--- src/main/java/com/api/pizzariamassagiga/repositories/RefeicaoRepository.java
+++ src/main/java/com/api/pizzariamassagiga/repositories/RefeicaoRepository.java
@@ -3,11 +3,11 @@ package com.api.pizzariamassagiga.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.pizzariamassagiga.models.RefeicaoModel;
-import com.api.pizzariamassagiga.models.TipoRefeicaoModel;
+
 
 
 public interface RefeicaoRepository extends JpaRepository<RefeicaoModel, Long>{
 	
-	boolean existsByIdTipoRefeicao(TipoRefeicaoModel refeicao);
+	
 
 }
