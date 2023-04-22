c27e6e4d35660bc3f438aa9dfdcef4674f55cce6
muriloabreu <murilo.lilo@hotmail.com>
2023-04-21 21:45:37 -0300

    Criação Interface EntregaRepository

---
+++ src/main/java/com/api/pizzariamassagiga/repositories/EntregaRepository.java
@@ -0,0 +1,9 @@
+package com.api.pizzariamassagiga.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.pizzariamassagiga.models.EntregaModel;
+
+public interface EntregaRepository extends JpaRepository<EntregaModel, Long> {
+
+}
